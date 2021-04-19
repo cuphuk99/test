d3647cc8e597a38bab9a7d88673f92020ee80633
Vlad <cuphuk@ukr.net>
2021-04-19 20:46:41 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -32,11 +32,7 @@ public class Main {
                 map.put(array[i], value + 1 );
             }
         }
-
-
         Map<String, Integer> sorted = new LinkedHashMap<>();
-
-
         map.entrySet().stream()
                 .sorted(Map.Entry.<String,Integer>comparingByValue().reversed())
                 .forEachOrdered(entry -> sorted.put(entry.getKey(), entry.getValue()));
@@ -52,9 +48,13 @@ public class Main {
             sorted20 += entry.getKey() + " " + entry.getValue() + System.lineSeparator();
             if (counter == 20) break;
         }
+
         String sortedToString = sorted.toString();
         Files.write(Paths.get("D:\\BestBrain\\Java kurs\\output.txt"),
                 sorted20.getBytes(StandardCharsets.UTF_8));
+
+
+//---------------------------Proper names---------------------------------------------
 
 
         String newText = new String(Files.readAllBytes(Paths.get("D:\\BestBrain\\Java kurs\\harry.txt")));
@@ -68,23 +68,19 @@ public class Main {
             if((Character.isUpperCase(newArray[i].charAt(0)))){
                 properNames[counterForNames] = newArray[i];
                 counterForNames++;
-                //System.out.println(properNames[counterForNames]);
             }
-
         }
-        String[] names20 = new String[20];
 
+        String[] names20 = new String[20];
         for (int i = 0; i < 20; i++) {
             names20[i] = properNames[i];
             System.out.println(names20[i]);
         }
 
-
         List<String> newList = Arrays.stream(properNames).collect(Collectors.toList());
         newList.stream().limit(20).forEach(System.out::println);
 
         String output = "D:\\BestBrain\\Java kurs\\outputNames.txt";
-
         Files.write(Paths.get(output), Arrays.toString(names20).getBytes(StandardCharsets.UTF_8));
     }
 }
