79f4c69ad9ec36847aaeb7983f35b98c971a3575
cuphuk99 <cuphuk99@gmail.com>
2021-04-21 23:45:24 +0300

    1

--- src/Prefix.java
+++ src/Prefix.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 /**
  * @author Ishtukin Vlad
  * @version 1.0.0
@@ -6,45 +9,13 @@
  * @since 19.04.2021 - 20.47
  */
 
-public class Prefix {
-
-    public static String prefix(String[] array) {
-        int[] lengthOfWords = new int[array.length];
-        int min = array[0].length();
-        String prefix = array[0];
-        int counter = 0;
-        for (int i = 0; i < array.length; i++) {
-            lengthOfWords[i] = array[i].length();
-            if (lengthOfWords[i]<min){
-                prefix = array[i];
-                min = lengthOfWords[i];
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[j].startsWith(prefix)){
-                    counter++;
-                }
-            }
-            if (counter == array.length){
-                break;
-            }else {
-                prefix = prefix.substring(0, prefix.length()-1);
-
-            }
-            counter = 0;
-
-        }
-        return prefix;
-    }
+ class Prefix {
 
     public static void main(String[] args) {
-        String[] array = {"abc", "abcd", "abfce", "abcac"};
-
-        System.out.println(prefix(array));  // ab
-
-        String[] array2 = {"abc", "abcd", "abce", "abcac"};
-
-        System.out.println(prefix(array2)); // abc
+        byte a = 12;
+        byte b = 125;
+        System.out.println(c);
     }
+
+
 }
---
+++ src/first/df.java
@@ -0,0 +1,10 @@
+package first;/*
+ * @author Vlad Ishtukin
+ * @project Main.java
+ * @class df
+ * @version 1.0.0
+ * @since 21.04.2021 - 23.40
+ */
+
+public class df {
+}
